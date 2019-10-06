4548415282253c90bbc813e1e2b759cd490a8ab4
İbrahim EDİZ <ibrahim.ediz@gazi.edu.tr>
2019-10-06 12:10:38 +0200

    06102019

--- Blog/urls.py
+++ Blog/urls.py
@@ -4,6 +4,8 @@ from . import views
 urlpatterns = [
 
     path('',views.Listele,name="gListele"),
-    path('detay/<int:pk>/',views.gDetay,name="gDetay")
+    path('detay/<int:pk>/',views.gDetay,name="gDetay"),
+    path('yeniGonderi/',views.yGonderi,name="yGonderi"),
+    path('dGonderi/<int:pk>/duzenle/',views.gDuzenle,name="gDuzenle")
 
 ]
--- Blog/views.py
+++ Blog/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render,get_object_or_404
+from django.shortcuts import render,get_object_or_404,redirect
 from .models import GonderiModel
+from .forms import GonderiForm
+from django.contrib.auth.models import User
+from django.utils import timezone
 
 def Listele(request):
     Gonderi = GonderiModel.objects.all()
@@ -9,3 +12,33 @@ def Listele(request):
 def gDetay(request,pk):
     gonderi = get_object_or_404(GonderiModel,pk=pk)
     return render(request,'Blog/gDetay.html',{"gonderi":gonderi})
+
+def yGonderi(request):
+    if request.method == "POST":
+        form = GonderiForm(request.POST)
+        if form.is_valid():
+            gonderi = form.save(commit=False)
+            ben = User.objects.get(username="admin")
+            gonderi.yazar = ben
+            gonderi.yayim_zaman = timezone.now()
+            gonderi.save()
+            return redirect('gDetay',pk=gonderi.pk)
+    else:
+        form = GonderiForm()
+    return render(request,'Blog/yGonderi.html',{"form":form})
+
+
+def gDuzenle(request,pk):
+    gonderi = get_object_or_404(GonderiModel,pk=pk)
+    if request.method == "POST":
+        form = GonderiForm(request.POST,instance=gonderi)
+        if form.is_valid():
+            gonderi = form.save(commit=False)
+            ben = User.objects.get(username="admin")
+            gonderi.yazar = ben
+            gonderi.yayim_zaman = timezone.now()
+            gonderi.save()
+            return redirect('gDetay',pk=gonderi.pk)
+    else:
+        form = GonderiForm(instance=gonderi)
+    return render(request,'Blog/dGonderi.html',{"form":form})
