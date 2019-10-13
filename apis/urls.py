9cac3a586dfae95d5bfa143dc9ff90c4cc1f78cb
İbrahim EDİZ <KURS>
2019-10-13 10:20:47 +0200

    13102019

--- apis/urls.py
+++ apis/urls.py
@@ -1,8 +1,15 @@
 from django.urls import path
-from .views import ListToDo,DetailToDo
+# from .views import ListToDo,DetailToDo
 
 
-urlpatterns = [
-    path('',ListToDo.as_view()),
-    path('<int:pk>/',DetailToDo.as_view())
-]
+# urlpatterns = [
+#     path('',ListToDo.as_view()),
+#     path('<int:pk>/',DetailToDo.as_view())
+# ]
+
+from .views import TodoViewSet
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+router.register('',TodoViewSet,base_name='todos')
+urlpatterns=router.urls
--- apis/views.py
+++ apis/views.py
@@ -1,13 +1,18 @@
 from django.shortcuts import render
-from rest_framework import generics
+# from rest_framework import generics
+from rest_framework import viewsets
 from todolist import models
 from .serializers import todoSerializer
 
 
-class ListToDo(generics.ListCreateAPIView):
-    queryset = models.todolist.objects.all()
-    serializer_class = todoSerializer
+# class ListToDo(generics.ListCreateAPIView):
+#     queryset = models.todolist.objects.all()
+#     serializer_class = todoSerializer
 
-class DetailToDo(generics.RetrieveDestroyAPIView):
+# class DetailToDo(generics.RetrieveUpdateDestroyAPIView):
+#     queryset = models.todolist.objects.all()
+#     serializer_class = todoSerializer
+
+class TodoViewSet(viewsets.ModelViewSet):
     queryset = models.todolist.objects.all()
-    serializer_class = todoSerializer
+    serializer_class = todoSerializer    
